d36263134fb563a5263b26482a0ff77fcffbd8c0
consultant <consultant-1379@tutamail.com>
2024-08-28 12:12:54 +0800

    first commit

---
+++ un-vsn-ci-model-jar/src/main/java/com/ericsson/mediation/model/ci/unvsn/package-info.java
@@ -0,0 +1,26 @@
+/*------------------------------------------------------------------------------
+ *******************************************************************************
+ * COPYRIGHT Ericsson 2022
+ *
+ * The copyright to the computer program(s) herein is the property of
+ * Ericsson Inc. The programs may be used and/or copied only with written
+ * permission from Ericsson Inc. or in accordance with the terms and
+ * conditions stipulated in the agreement/contract under which the
+ * program(s) have been supplied.
+ *******************************************************************************
+ *----------------------------------------------------------------------------*/
+/**
+ * <p>
+ * The UN_VSN_MED namespace contains packages for UN-VSN based info.
+ * </p>
+ * The element defined in this package is <code>UnVsnConnectivityInformation</code>.
+ * </p>
+ * <p>
+ * Class names have the current version appended to prevent overwrites if amendments later.
+ * </p>
+ *
+ * @see com.ericsson.mediation.model.ci.unvsn.UnVsnConnectivityInformation_100.java
+ *
+ *
+ **/
+package com.ericsson.mediation.model.ci.unvsn;
